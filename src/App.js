82d13c0562af91c2ed93b41cf382c17a9bc283a2
Tuấn Nguyễn Đình Anh <tuan.nda@teko.vn>
2020-12-23 18:36:54 +0700

    overview component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./containers/LandingPage/home/Home";
-import AboutUs from "./containers/LandingPage/about/AboutUs";
-import Blog from "./containers/LandingPage/blog/Blog";
-import ContactUs from "./containers/LandingPage/contact/ContactUs";
 import Login from "./containers/Login/Login";
-import Header from "./containers/LandingPage/Header";
-import Footer from "./containers/LandingPage/Footer";
-import "antd/dist/antd.css";
-import Register from "./containers/Register/Register";
-import ForgotPassword from "./containers/Forgot_Password/Forgot_Passsword";
-import Logged from "./Logged";
+// import "antd/dist/antd.css";
+import AppLayout from "./containers/AppLayout/AppLayout";
+import React from "react";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Header index={1} />
-          <Home />
-          <Footer />
-        </Route>
-        <Route path="/aboutus">
-          <Header index={1} />
-          <AboutUs />
-          <Footer />
-        </Route>
-        <Route path="/blog">
-          <Header index={1} />
-          <Blog />
-          <Footer />
-        </Route>
-        <Route path="/contactus">
-          <Header index={1} />
-          <ContactUs />
-          <Footer />
-        </Route>
-        <Route path="/login">
-          <Header index={1} />
-          <Login />
-          <Footer />
-        </Route>
-        <Route path="/register">
-          <Header index={1} />
-          <Register />
-          <Footer />
-        </Route>
-        <Route path="/forgot_password">
-          <Header index={1} />
-          <ForgotPassword />
-          <Footer />
-        </Route>
-        <Route path="/Logged">
-          <Logged />
-        </Route>
+          <Route exact path="/" name="/" component={AppLayout} />
+          <Route exact path="/login" name="login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/containers/AppLayout/AppLayout.js
@@ -0,0 +1,61 @@
+import { Layout, Menu } from 'antd';
+import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+import './AppLayout.css';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const AppLayout = () => {
+    return (
+        <Router>
+            <Layout style={{height:"100vh"}}>
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={broken => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                >
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1" icon={<UserOutlined />}>
+                            <Link to={'/images'}>
+                                Images
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                            Albums
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<UploadOutlined />}>
+                            Sharing
+                        </Menu.Item>
+                        <Menu.Item key="4" icon={<UserOutlined />}>
+                            Trash
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div className="site-layout-background" style={{ padding: 24 }}>
+                            <Switch>
+                                <Route path="/images">
+                                    <div>Something</div>
+                                </Route>
+                                <Route path="/next-item">
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Content>
+                    {/*<Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>*/}
+                </Layout>
+            </Layout>
+        </Router>
+    )
+}
+
+export default AppLayout;
