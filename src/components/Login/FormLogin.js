20a5e07470d7291e8401296a65ee7122585138ac
Tuấn Nguyễn Đình Anh <tuan.nda@teko.vn>
2020-12-28 01:44:18 +0700

    update login

--- src/components/Login/FormLogin.js
+++ src/components/Login/FormLogin.js
@@ -1,96 +1,129 @@
-
-import { Form, Input, Button, Checkbox } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Form, Input, Button, Checkbox, AutoComplete } from "antd";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 import "./FormLogin";
 import { Link } from "react-router-dom";
-import {useState} from 'react'
-import {authServices} from "../../services";
+import { useState } from "react";
+import logo1 from "./../../assets/logo1.png";
+import { authServices } from "../../services";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleOnChangeUsername = (e) => {
-    setUsername(e.target.value)
-  }
-
-  const handleOnChangePassword = (e ) => {
-    setPassword(e.target.value)
-  }
-
-  const onFinish = () => {
-    authServices.login().then(
-        res => {
-          console.log(res.data)
-          authServices.setAccessToken(res.data)
-        }
-    ).catch(
-        err => console.log(err)
-    )
+    setUsername(e.target.value);
   };
 
+  const handleOnChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
- 
+  const onFinish = () => {
+    authServices
+        .login()
+        .then((res) => authServices.setAccessToken(res.data))
+        .catch((err) => console.log(err));
+  };
+
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  const onEmailChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(
+          ["@gmail.com", "@vnu.edu.vn", "@yahoo.com"].map(
+              (domain) => `${value}${domain}`
+          )
+      );
+    }
+  };
+
+  const emailOptions = autoCompleteResult.map((email) => ({
+    label: email,
+    value: email,
+  }));
 
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
+      <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
       >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Username"
-          value={username}
-          onChange={handleOnChangeUsername}
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handleOnChangePassword}
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="/forgot_password">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button" >
-          Log in
-        </Button>
-        Or <a href="/register"> register now!</a>
-      </Form.Item>
-    </Form>
+        <div>
+          <div className="Logo">
+            <img src={logo1} alt="logo" width="270" height="250" />
+          </div>
+          <Form.Item
+              name="username"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+              ]}
+              hasFeedback
+          >
+            <div>
+              <Input
+                  className="form-box"
+                  prefix={<MailOutlined className="site-form-item-icon" />}
+                  placeholder="Email"
+                  value={username}
+                  onChange={handleOnChangeUsername}
+              />
+            </div>
+          </Form.Item>
+          <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+                {
+                  required: true,
+                  pattern: new RegExp("^\\S*$"),
+                  message: "No whitespace please!",
+                },
+              ]}
+              hasFeedback
+          >
+            <Input.Password
+                className="form-box"
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleOnChangePassword}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+            >
+              Log in
+            </Button>
+          </Form.Item>
+          <div>
+            <Form.Item>
+              Or <a href="/register"> register now!</a>
+              <a className="login-form-forgot" href="/forgot_password">
+                Forgot password
+              </a>
+            </Form.Item>
+          </div>
+        </div>
+      </Form>
   );
 };
 
